1cccaee16ac012713dbe3d26b8d7cd1650886a0d
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2023-11-07 16:10:59 -0500

    Initial Commit

---
+++ FocusFarmSwiftUI/FocusFarmSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFarmSwiftUIApp.swift
+//  FocusFarmSwiftUI
+//
+//  Created by Jacob Martinage on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusFarmSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
